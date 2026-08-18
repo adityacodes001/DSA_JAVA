a95f9cac3b5c47750e0aa18ce7d06de1c6d823aa
aditya maurya <adityacodes01@gmail.com>
2026-08-19 01:45:59 +0530

    Added BasicSorting

--- BasicSorting.java
+++ BasicSorting.java
@@ -1,28 +1,32 @@
 import java.util.*;
 public class BasicSorting {
-  public static void  bubbleSort(int [] arr){
-      for(int turn =0; turn<arr.length-1; turn++){
-          for(int j =0; j<arr.length-1-turn; j++){
-              if(arr[j] > arr[j+1]){
+    public static void  bubbleSort(int [] arr){
+        for(int turn =0; turn<arr.length-1; turn++){
+            for(int j =0; j<arr.length-1-turn; j++){
+                if(arr[j] > arr[j+1]){
 
-                  // swapping ---
-                  int temp = arr[j];
-                  arr[j] = arr[j+1];
-                  arr[j+1] = temp;
-              }
-          }
-      }
-  }
-   public static void printArr(int []arr){
-      for(int i=0; i<arr.length; i++){
-          System.out.print(arr[i]+" ");
-      }
-       System.out.println();
-   }
+                    // swapping ---
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void printArr(int []arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
         int [] arr = {3,6,2,1,8,7,4,5,3,1};
         bubbleSort(arr);
         printArr(arr);
+
+
+
+
     }
 }
