bfed15711160fae2ee225e833066fa38c93dec09
aditya maurya <adityacodes01@gmail.com>
2026-08-19 00:04:51 +0530

    Added BasicSorting

---
+++ stoneGame.java
@@ -0,0 +1,10 @@
+public class stoneGame {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5 ) {
+                continue; // skip when i is 5 -----
+            }
+            System.out.println(i);
+        }
+    }
+ }
